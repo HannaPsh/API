ec721ec53a7bfb27621d87a7303c8e83d4a0e22f
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-03-23 16:10:33 +0100

    Backend Ehandel

---
+++ routes/products.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+
+// GET all posts
+router.get('/', async (req, res) => {
+  try {
+    res.json(await Product.find());
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.get('/:productId', async (req, res) => {
+  try {
+    res.json(await Product.findById(req.params.productId));
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const product = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    image: req.body.image,
+    price: req.body.price,
+    availableSizes: req.body.availableSizes,
+  });
+  try {
+    const savedProduct = await product.save();
+    res.json(savedProduct);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Delete post
+router.delete('/:productId', async (req, res) => {
+  try {
+    const deletedProduct = await Product.deleteOne({
+      _id: req.params.productId,
+    });
+    res.json(deletedProduct);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Update post
+router.patch('/:productId', async (req, res) => {
+  try {
+    const updatedProduct = await Product.updateOne(
+      { _id: req.params.productId },
+      {
+        $set: {
+          title: req.body.title,
+          description: req.body.description,
+          image: req.body.image,
+          price: req.body.price,
+          availableSizes: req.body.availableSizes,
+        },
+      }
+    );
+    res.json(updatedProduct);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+module.exports = router;
